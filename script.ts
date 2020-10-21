ec5ec5b1bbea675c160dc1adab4a9370a7b23b75
Hernan <hernanastudillom@gmail.com>
2020-10-21 17:56:47 -0300

    Push Quiz Web 1

---
+++ script.ts
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    var forms = document.getElementsByClassName('needs-validation');
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+          alert('Por favor revise los campos en rojo.');
+        }
+        else{
+          document.getElementById("textoSuccess").innerHTML = "Datos Entregados!";
+          alert('Datos entregados con éxito!');
+          event.preventDefault();  /* Para apreciar el texto nuevo y no recargar la pagina */
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+});
+
+function habilitarDNI(){
+  var radios = document.querySelectorAll('[customRadioInline1]');
+  Array.from(radios).forEach(function(r){
+    r.addEventListener('click', function(){
+      var elemento = document.getElementById('RUTSubmit');
+      if(this.id == 'RUTSubmit')
+        elemento.removeAttribute('disabled');
+      else
+        elemento.setAttribute('disabled', 'disabled');
+    });
+});
+}
